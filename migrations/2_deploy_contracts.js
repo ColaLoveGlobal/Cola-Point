3b93913cb569e484ff10badf3be265d3de0ad949
BTCraftman-Token <39944955+RoyalYao@users.noreply.github.com>
2020-08-05 08:51:00 +0800

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 const ConvertLib = artifacts.require("ConvertLib");
-const JZC_token = artifacts.require("JZC_token");
+const JZC_Token = artifacts.require("JZC_Token");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, JZC_token);
-  deployer.deploy(JZC_token);
+  deployer.link(ConvertLib, JZC_Token);
+  deployer.deploy(JZC_Token);
 };
